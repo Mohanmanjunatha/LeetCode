277b3caa41299eabc4323a6100c238d19693c953
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-07-06 19:34:43 -0400

    Time: 2 ms (54.53%), Space: 41.5 MB (37.55%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,32 +1,32 @@
 class Solution {
-     
+
     public boolean isValid(String s) {
-        
+
         HashMap<Character, Character> mappedBrackets = new HashMap<>();
-        
-        mappedBrackets.put(')','(');
-        mappedBrackets.put('}','{');
-        mappedBrackets.put(']','[');
-        
-        Stack<Character>  stack = new Stack<>();
-        
-        for(int i=0; i<s.length(); i++){
+
+        mappedBrackets.put(')', '(');
+        mappedBrackets.put('}', '{');
+        mappedBrackets.put(']', '[');
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            
-            if(!mappedBrackets.containsKey(c)){
-                stack.push(c);
-            }
-            else{
-                if(stack.isEmpty()){
+
+            if (!mappedBrackets.containsKey(c)) { // Checking if it's an open bracket, then add it to stack
+                stack.push(c); // If it's not open, then it must be a closed bracket
+            } else {
+                // We check here since to push a closed bracket, there must be an open present. If not, exit
+                if (stack.isEmpty()) {
                     return false;
                 }
+
                 char topElement = stack.pop();
-                if(topElement != mappedBrackets.get(c)){
+                if (topElement != mappedBrackets.get(c)) { // Use equality operator here
                     return false;
                 }
             }
         }
-        
-        return stack.isEmpty();
+        return stack.isEmpty(); // It should be empty at the end
     }
 }
