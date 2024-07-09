993ad511186fbe04484fcd5b077eaad68da19936
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-07-09 15:21:56 -0400

    Time: 0 ms (100.00%), Space: 42.1 MB (89.02%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,18 +1,38 @@
-class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//create 3 list
+// craete two pointers
+//compare each pointer and check which one is less and add it to 3rd list
+// base case when both aree of same add them both and icremeent both the pointers 
+//we create a dummy node since we need to return the head
+//we need to assign this as head and then start adding values to the third list
 
-        if(list1!=null && list2!=null){
-        if(list1.val<list2.val){
-            list1.next=mergeTwoLists(list1.next,list2);
-            return list1;
+
+class Solution{
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
+        ListNode Dummy=new ListNode(1);
+        ListNode merge=Dummy;
+        
+        while(list1!=null && list2!=null){
+            if(list1.val<=list2.val){
+                merge.next=list1;
+                list1=list1.next;
+                
             }
             else{
-                list2.next=mergeTwoLists(list1,list2.next);
-                return list2;
+                merge.next=list2;
+                list2=list2.next;
+                
+            }
+            merge=merge.next;
         }
+        
+        if (merge.next==list1 && list1==null){
+            merge.next=list2;
+            
         }
-        if(list1==null)
-            return list2;
-        return list1;
+        else{
+            merge.next=list1;
+        }
+        return Dummy.next;
     }
+    
 }
