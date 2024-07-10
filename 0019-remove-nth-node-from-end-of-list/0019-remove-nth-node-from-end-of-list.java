01549a227f37bd889f5093d49407b05b9a3556e6
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-07-10 08:13:27 -0400

    Time: 0 ms (100.00%), Space: 41.9 MB (9.97%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,34 +1,27 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+//bruteforce
+//navigate to find the length and use formula l-n+1
+//we navigate twice to find the length and to navigate for deletion
+// optimized
+//creating a dummy node and assiging both pointers to it
+//creating a difference between front and back  using for loop
+// traverse through the list by incrementing both using while
+//at last when u find null swap the back.next with back.next.next
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         
  
         ListNode Dummy = new ListNode(1);
         Dummy.next=head;
-        
-        
-        ListNode front=Dummy;
+         ListNode front=Dummy;
         ListNode back=Dummy;
-        for (int i=0;i<=n;i++){
+        for (int i=0;i<=n;i++){// moving front pointer 2 steps forward 
             front=front.next;
-        }
-        
-        
-        while(front!=null){
+        } 
+        while(front!=null){// traversing through the list
             front=front.next;
-            back=back.next;
-            
+            back=back.next; 
         }
-        back.next=back.next.next;
+        back.next=back.next.next;//now swapping the referrences 
         return Dummy.next;
         
     }
