1a11320d12dd9d96773005f583fe55f490adc10b
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-07-12 18:13:48 -0400

    Time: 6 ms (68.38%), Space: 44.7 MB (36.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,23 +1,64 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int maxLength = 0;
-        Set<Character> charSet = new HashSet<>();
-        int left = 0;
         
-        for (int right = 0; right < n; right++) {
-            if (!charSet.contains(s.charAt(right))) {
-                charSet.add(s.charAt(right));
-                maxLength = Math.max(maxLength, right - left + 1);
-            } else {
-                while (charSet.contains(s.charAt(right))) {
-                    charSet.remove(s.charAt(left));
-                    left++;
-                }
-                charSet.add(s.charAt(right));
-            }
+        if(s == null || s.length()==0){
+            return 0;
         }
         
-        return maxLength;
+        if(s.length() == 1){
+            return 1;
+        }
+        
+        int left = 0;
+        int right = 0;
+        int ans = 0;
+        
+        HashSet<Character> set = new HashSet<>();
+        
+        while(right < s.length()){
+            char c = s.charAt(right);
+            while(set.contains(c)){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(c);
+            ans = Math.max(ans, right-left + 1);
+            right++;
+        }
+        
+        return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int n = s.length();
+//         int maxLength = 0;
+//         Set<Character> charSet = new HashSet<>();
+//         int left = 0;
+        
+//         for (int right = 0; right < n; right++) {
+//             if (!charSet.contains(s.charAt(right))) {
+//                 charSet.add(s.charAt(right));
+//                 maxLength = Math.max(maxLength, right - left + 1);
+//             } else {
+//                 while (charSet.contains(s.charAt(right))) {
+//                     charSet.remove(s.charAt(left));
+//                     left++;
+//                 }
+//                 charSet.add(s.charAt(right));
+//             }
+//         }
+        
+//         return maxLength;
+//     }
+// }
