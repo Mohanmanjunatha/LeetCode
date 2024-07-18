0b3e3e91290100e595125ba2da2a6d95a28b53f4
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-07-18 07:56:16 -0400

    Time: 0 ms (100.00%), Space: 40.9 MB (49.32%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -20,6 +20,33 @@
     
     
 // }
+// class Solution{
+
+// public int rob(int[] nums) {
+
+//  if (nums == null || nums.length == 0) return 0;
+
+//  if (nums.length == 1) return nums[0];
+
+//  if (nums.length == 2) return Math.max(nums[0], nums[1]);
+
+//  int[] dp = new int[nums.length];
+
+//  dp[0] = nums[0];
+
+//  dp[1] = Math.max(nums[0], nums[1]);
+
+//  for (int i = 2; i < nums.length; i++) {
+
+//  dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+
+//  }
+
+//  return dp[nums.length - 1];
+
+//  }
+// }
+
 class Solution{
 
 public int rob(int[] nums) {
@@ -30,21 +57,24 @@ public int rob(int[] nums) {
 
  if (nums.length == 2) return Math.max(nums[0], nums[1]);
 
- int[] dp = new int[nums.length];
+int n = nums.length;
 
- dp[0] = nums[0];
+ int prev = nums[0];
 
- dp[1] = Math.max(nums[0], nums[1]);
+ int curr = Math.max(nums[0], nums[1]);
 
  for (int i = 2; i < nums.length; i++) {
 
- dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+int temp=curr;
+
+curr=Math.max(curr, prev+nums[i]);
+
+prev=temp;
 
  }
 
- return dp[nums.length - 1];
+ return curr;
 
  }
 }
-
  
