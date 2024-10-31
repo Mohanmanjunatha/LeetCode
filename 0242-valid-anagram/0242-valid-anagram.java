1b2d22d008b7a1eb664236e45aee2f4e68b7e891
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-10-31 16:38:09 -0400

    Time: 5 ms (54.29%), Space: 44.7 MB (19.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,32 +1,17 @@
-//sol1
-//sort each string and compare them
-//TC=O(nlogn)
-
-//base case
-//make sure both srings have same length
-//creeate a hashmap adda the key value pairs
-//compare both of them and they both should have same values for each key
-//TC=O(S+T), SC=O(S+T)
-
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
+
         if(s.length()!=t.length()){
-            return false;   
+            return false;
+
         }
-        int[] store= new int[26];
-    
-        for (int i=0;i<s.length();i++){
-            store[s.charAt(i)-'a']++;
-            store[t.charAt(i)-'a']--;  
-        }
-        for(int i=0;i<store.length;i++)
-        {
-            if (store[i]!=0) 
-        return false;     
-}
-        return true;
-        
+        char [] ssort= s.toCharArray();
+        char [] tsort= t.toCharArray();
+
+        Arrays.sort(ssort);
+        Arrays.sort(tsort);
+        return Arrays.equals(ssort,tsort);
+
         
     }
 }
