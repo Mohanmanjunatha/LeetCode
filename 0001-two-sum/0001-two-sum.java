6e7626aaa31c5f940e4325219f38587f258a6aea
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-11-01 19:00:16 -0400

    Time: 2 ms (98.86%), Space: 44.9 MB (31.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,17 +1,65 @@
-class Solution{
-    public int[] twoSum(int[] nums, int target){
-        
-        Map<Integer, Integer> map=new HashMap<>();
-        for (int i=0;i<nums.length;i++){
-            int diff = target-nums[i];
 
-        if(map.containsKey(diff)){
-            return new int []{map.get(diff),i};
-        }
-        map.put(nums[i],i);
-        
-            
-        }
-        return null;
-    }
+// class Solution{
+//     public int[] twoSum(int[] nums, int target){
+//         for(int j=0;j<nums.length;j++){
+//             for (int i=j+1;i<nums.length;i++)
+//             {
+//                 int sum=nums[i]+nums[j];
+//                 if(sum==target){
+//                     return new int[]{i,j};
+//                 }
+//             }
+//         }
+
+//         return new int[]{};
+//     }
+// }
+
+// //Using HashMap
+
+// class Solution{
+    
+// }
+
+class Solution {
+
+ 
+
+public int[] twoSum(int[] nums, int target) {
+
+ 
+
+HashMap<Integer, Integer> map = new HashMap<>();
+
+ 
+
+for (int i = 0; i < nums.length; i++) 
+
+{
+
+int num = nums[i];
+
+int diff = target - num;
+
+if (map.containsKey(diff)) 
+
+{
+
+return new int[] { map.get(diff), i };
+
 }
+
+map.put(num, i);
+
+}
+
+return new int[] {};
+
+}
+
+}
+
+
+
+
+
