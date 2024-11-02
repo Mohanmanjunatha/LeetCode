3020a10b94a74792e406fe1464a55f0c0dd3815d
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-11-01 22:22:03 -0400

    Time: 1 ms (84.1%), Space: 41 MB (99.69%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,25 +1,51 @@
-class Solution {
-    public String mergeAlternately(String word1, String word2) {
-    int m=word1.length();
-    int n=word2.length();
+// since we have ti
 
-    char[] s1= word1.toCharArray();
-    char[] s2= word2.toCharArray();
-    StringBuilder result=new StringBuilder();
-     int i=0;
-     int j=0;
-     while(i<m||j<n){
-        if(i<m){
-            result.append(word1.charAt(i++));
+// class Solution {
+//     public String mergeAlternately(String word1, String word2) {
+//     int m=word1.length();
+//     int n=word2.length();
 
-        }
-        if (j<n){
-            result.append(word2.charAt(j++));
-        }
+//     char[] s1= word1.toCharArray();
+//     char[] s2= word2.toCharArray();
+//     StringBuilder result=new StringBuilder();
+//      int i=0;
+//      int j=0;
+//      while(i<m||j<n){
+//         if(i<m){
+//             result.append(word1.charAt(i++));
+
+//         }
+//         if (j<n){
+//             result.append(word2.charAt(j++));
+//         }
         
-     }
+//      }
    
-     return result.toString();
+//      return result.toString();
         
-    }
+//     }
+// }
+
+
+class Solution{
+     public String mergeAlternately(String word1, String word2){
+
+        char[] s1=word1.toCharArray();
+        char[] s2=word2.toCharArray();
+        int m=word1.length();
+        int n=word2.length();
+        StringBuilder result=new StringBuilder();
+
+        for (int i=0;i<Math.max(m,n);i++){
+
+            if (i<m){
+                result.append(s1[i]);
+            }
+            if (i<n){
+                result.append(s2[i]);
+            }
+        }
+        return result.toString();
+     }
+     
 }
