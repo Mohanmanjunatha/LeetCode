e2d5effcd6e1c0665e9fe7413caa4cb6dbd4c630
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-11-03 11:45:52 -0500

    Time: 32 ms (5.57%), Space: 45 MB (5.26%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,28 @@
 class Solution {
     public boolean isValid(String s) {
 
-                if (s.isEmpty()) {
+        if (s.isEmpty())
+        {
             return true;
         }
 
-        // Loop until no more pairs can be found
+
         int length;
         do {
-            length = s.length();
-            // Remove all pairs from the string
-            s = s.replace("()", "")
-                 .replace("{}", "")
-                 .replace("[]", "");
-        } while (length != s.length());  // Continue until length no longer changes
+            length=s.length();
+            s=s.replace("()","")
+                .replace("[]", "")
+                .replace("{}","");
 
-        // If the final string is empty, it means all pairs were matched correctly
+        }while(length!=s.length());
+
         return s.isEmpty();
-    }
+
+
+
+
         
-    
-}
+        }
+    }
+
+
