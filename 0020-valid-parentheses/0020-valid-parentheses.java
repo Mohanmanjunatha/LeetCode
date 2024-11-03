7fca8f6ecfda0d09261f8eaa468630559c415491
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-11-03 12:15:43 -0500

    Time: 3 ms (44.93%), Space: 41.4 MB (76%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,36 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         if (s.isEmpty()){
+//             return true;
+//         }
+//         int length;
+//         do {
+//             length=s.length();
+//             s=s.replace("()","")
+//                 .replace("[]", "")
+//                 .replace("{}","");
+//         }
+//         while(length!=s.length());
+//         return s.isEmpty();  
+//         }
+//     }
+
 class Solution {
     public boolean isValid(String s) {
-
-        if (s.isEmpty())
-        {
-            return true;
+        HashMap<Character,Character> maps=new HashMap<Character,Character>();
+        maps.put(')','(');
+        maps.put(']','[');
+        maps.put('}','{');
+        Stack<Character> stack=new Stack<Character>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(maps.containsKey(c)){
+                if(stack.empty()||stack.pop()!=maps.get(c))return false;
+            }
+            else
+                stack.push(c);
         }
-
-
-        int length;
-        do {
-            length=s.length();
-            s=s.replace("()","")
-                .replace("[]", "")
-                .replace("{}","");
-
-        }while(length!=s.length());
-
-        return s.isEmpty();
-
-
-
-
-        
-        }
+        return stack.empty();
     }
-
+}
 
