41c02c4ee1083cc58bcedca93a3a9a4d5acd67fd
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-11-03 20:40:36 -0500

    Time: 3 ms (44.73%), Space: 41.9 MB (16.15%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,36 +1,53 @@
 // class Solution {
 //     public boolean isValid(String s) {
-//         if (s.isEmpty()){
-//             return true;
-//         }
-//         int length;
-//         do {
-//             length=s.length();
-//             s=s.replace("()","")
-//                 .replace("[]", "")
-//                 .replace("{}","");
-//         }
-//         while(length!=s.length());
-//         return s.isEmpty();  
-//         }
+        // if (s.isEmpty()){
+        //     return true;
+        // }
+        // int length;
+        // do {
+        //     length=s.length();
+        //     s=s.replace("()","")
+        //         .replace("[]", "")
+        //         .replace("{}","");
+        // }
+        // while(length!=s.length());
+        // return s.isEmpty();  
+        // }
 //     }
+//Optimized
+//checking if its open bracket then add it to stack
+// if its not open then it must be close bracket
+//we check here since to push a closed bracket there must be open present if not then exit
+//it should be empty at the end
 
 class Solution {
     public boolean isValid(String s) {
-        HashMap<Character,Character> maps=new HashMap<Character,Character>();
-        maps.put(')','(');
-        maps.put(']','[');
-        maps.put('}','{');
-        Stack<Character> stack=new Stack<Character>();
-        for(int i=0;i<s.length();i++){
+
+        HashMap<Character,Character> map=new HashMap<>();
+        map.put('}','{');
+        map.put(')','(');
+        map.put(']','[');
+
+        Stack<Character> stack=new Stack<>();
+        for (int i=0;i<s.length();i++){
             char c=s.charAt(i);
-            if(maps.containsKey(c)){
-                if(stack.empty()||stack.pop()!=maps.get(c))return false;
-            }
-            else
+            if (!map.containsKey(c)){
                 stack.push(c);
+                
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top=stack.pop();
+                if (top!=map.get(c)){
+                    return false;
+                }
+            }
         }
-        return stack.empty();
+
+       return stack.isEmpty();
     }
 }
+
 
