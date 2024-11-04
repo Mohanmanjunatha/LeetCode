a0e95a97714e9e6f409377e32e1f017e4154b0f3
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-11-03 22:38:32 -0500

    Time: 50 ms (34.81%), Space: 127.5 MB (11.07%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,40 +1,79 @@
 class LRUCache {
-    class Node{
-        int key;
-        int value;
-        public Node(int key, int value){
-            this.key=key;
-            this.value=value;
+
+    private Map<Integer, Node> cache;
+    private int capacity;
+
+    private Node left;
+    private Node right;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        cache = new HashMap<>();
+
+        //left = LRU , right = most recent
+        this.left = new Node(0, 0);
+        this.right = new Node(0, 0);
+        this.left.next = this.right;
+        this.right.prev = this.left;
+    }
+
+    public int get(int key) {
+        if (cache.containsKey(key)) {
+            remove(cache.get(key));
+            insert(cache.get(key));
+            return cache.get(key).val;
+        } else {
+            return -1;
         }
     }
 
-    LinkedList<Node> list;
-    Map<Integer, Node> map;
-    int capacity;
-    public LRUCache(int capacity) {
-        this.capacity=capacity;
-        map=new HashMap<>();
-        list=new LinkedList<>();
-    }
-    
-    public int get(int key) {
-        if(map.containsKey(key)){
-            Node cur=map.get(key);
-            list.remove(cur);
-            list.addFirst(cur);
-            map.put(key, cur);
-            return cur.value;
-        }
-        return -1;
-    }
-    
     public void put(int key, int value) {
-        if(map.containsKey(key)) list.remove(map.get(key));
-        list.addFirst(new Node(key, value));
-        map.put(key, list.peekFirst());
-        if(list.size()>capacity){
-            int deletedKey=list.removeLast().key;
-            map.remove(deletedKey);
+        if (cache.containsKey(key)) {
+            remove(cache.get(key));
+        }
+        cache.put(key, new Node(key, value));
+        insert(cache.get(key));
+
+        if (cache.size() > capacity) {
+            // remove from the list and delte the LRU from the hashmap
+            Node lru = this.left.next;
+            remove(lru);
+            cache.remove(lru.key);
+        }
+    }
+
+    // remove node from list
+    public void remove(Node node) {
+        Node prev = node.prev;
+        Node next = node.next;
+
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    // insert node at right
+    public void insert(Node node) {
+        Node prev = this.right.prev;
+        Node next = this.right;
+
+        prev.next = node;
+        next.prev = node;
+
+        node.next = next;
+        node.prev = prev;
+    }
+
+    private class Node {
+
+        private int key;
+        private int val;
+
+        Node next;
+        Node prev;
+
+        public Node(int key, int val) {
+            this.key = key;
+            this.val = val;
         }
     }
 }
