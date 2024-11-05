b71f98e10d7114a67f80d72b93afbe863d935a8a
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-11-05 14:06:53 -0500

    Time: 143 ms (5.46%), Space: 44.2 MB (28%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,20 +1,18 @@
+// Example with 12321:
+// After reversing half, we get x = 12 and revertedNumber = 123.
+// Since x == revertedNumber / 10 (12 == 123 / 10), we confirm it’s a palindrome.
 
-
-
-class Solution{
-    public boolean isPalindrome(int x){
-        if(x<0){
-            return false;
-            
-        }
-        long reversed=0;
-        long temp=x;
-        while(temp!=0){
-            int digit =(int)(temp%10);
-            reversed=reversed*10+digit;
-            temp=temp/10;
-            
-        }
-        return (reversed==x);
+class Solution {
+    public boolean isPalindrome(int x) {
+       if (x<0 || (x%10 ==0 && x!=0)){
+        return false;
+       }
+       int reverse=0;
+       while(x>reverse){
+        reverse=reverse*10+(x%10);
+        x=x/10;
+       }
+       return x==reverse||x==reverse/10;
+        
     }
 }
