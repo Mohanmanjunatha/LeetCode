935543cf60b4de22756132705f5ab4b191aeeebf
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-11-05 18:28:35 -0500

    Time: 1 ms (66.4%), Space: 45.5 MB (94.77%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,3 +1,12 @@
+//Trapping Rainwater
+// we use two arrays here to calculate left tallest for current block
+// we also calculate right tallest for each block to make sure the threshold till where the water can hold
+//we have to store these two in seperate arrays using max function and two loops from left and right side
+//once its done, from the picture we have for each block the height of left and right building
+//so from those 2 we take the minimum value and subtract the current block to find out the amount of water we can store 
+//at the end we add all these findings for each block to return the answer
+
+
 class Solution {
     public int trap(int[] height) {
           int n=height.length;
