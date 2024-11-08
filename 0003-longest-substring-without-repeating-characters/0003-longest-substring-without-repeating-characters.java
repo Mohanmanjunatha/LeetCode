adce0e3c2162f523b545659d70729913e8357be9
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-11-07 23:09:53 -0500

    Time: 6 ms (69.93%), Space: 44.6 MB (45.05%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,25 +1,30 @@
-class Solution {
+// here we create a HashSet to store the vallues
+//we need a variable to store the max length and a left pointer 
+// we will use array loop as right pointer 
+//for loop to access each element of the array
+// for each element check if that element is present 
+//if not present then we keep adding it using for loop and hashset and keep incrementing the max length
+// when we find the re4peated char then we should remove the like 
+//if we have abcbca and we find b repeated then we have to remove till we remove b and for that wile loop runs 2 times so l will be 2 pointing to next start window 
+// after removing wwe will add new character to it .
+// by this time we will have the maximum
+//max is right array-left pointer +1 its like an logest  string window or the length 
+
+
+public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        int maxLength = 0;
-        int start = 0;
+        HashSet<Character> charSet = new HashSet<>();
+        int l = 0;
+        int res = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            char ca = s.charAt(i);
-
-            // If character is already in the map, update the start to the next position after its last occurrence
-            if (map.containsKey(ca)) {
-                start = Math.max(start, map.get(ca) + 1);
+        for (int r = 0; r < s.length(); r++) {
+            while (charSet.contains(s.charAt(r))) {
+                charSet.remove(s.charAt(l));
+                l++;
             }
-
-            // Update the character's position in the map
-            map.put(ca, i);
-
-            // Calculate the length of the current substring and update maxLength
-            maxLength = Math.max(maxLength, i - start + 1);
+            charSet.add(s.charAt(r));
+            res = Math.max(res, r - l + 1);
         }
-
-        return maxLength;
+        return res;
     }
 }
-
