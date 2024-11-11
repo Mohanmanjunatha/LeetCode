071de1388fbb50683b5cbef9c9cb901ac61b8a5e
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-11-11 13:25:59 -0500

    Time: 16 ms (20.71%), Space: 48.1 MB (75.61%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,11 +1,20 @@
-import java.util.*;
+
+// use hashmap and priority queue
+// will do it through Hashmap 
+//we will checka and add the elements
+// we could have taken the freq of count from hashmap but think if we have all elements with same count ?
+// we will use priority queue to maintain the decending order
+//then we add the keys to queue
+//then will create an array to add the top frequesnt elements in that array and retrun
+
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> m = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int n = nums[i];
             if (!m.containsKey(n)) {
-                m.put(n, 1);
+                 m.put(n, 1);
             } else {
                 m.put(n, m.get(n) + 1);
             }
