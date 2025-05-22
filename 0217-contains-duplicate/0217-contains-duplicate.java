1b7be9bcdf308c3ae719264575f7005f4ede313f
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2025-05-22 16:08:09 -0700

    Time: 13 ms (69%), Space: 58.6 MB (36.88%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,29 +1,85 @@
-//1)//bruteforce 
-//tc=o(N*N)
-//sc=O(1)
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+  
+//      HashMap<Integer, Boolean> map =new HashMap<>();
 
-//Better Approach
 
-//2)//sort the array  and save time since we get result early 
+//      for (int i=0;i<nums.length;i++){
+//         int num=nums[i];
 
-//TC=O(nlogn)
-//sc=O(1)
+//         if (map.containsKey(num)){
+//             return true;
+//         }
 
-//3)//if we can compromise some space then hashset works better in O(1)
-//check in hashset if the element contains// if not add it
+//           map.put(num,true);
+//      }
 
+     
+//      return false;
+
+
+
+//     }
+//}
+// class Solution {
+// public boolean containsDuplicate(int[] nums) {
+//     Set<Integer> set = new HashSet<>(nums.length);
+//     for (int x: nums) {
+//         if (set.contains(x)) return true;
+//         set.add(x);
+//     }
+//     return false;
+// }
+// }
+
+//May22
+
+
+//bruteforce :
+
+// class Solution{
+//     public boolean containsDuplicate(int[] nums){
+
+//         for (int i=0;i<nums.length-1;i++){
+//             for (int j=i+1; j<nums.length;j++){
+//                 if (nums[i]==nums[j]){
+//                     return true;
+//                 }
+
+//             }
+
+//         }
+//         return false;
+//     }
+// }
+
+
+//Optimal
 class Solution{
     public boolean containsDuplicate(int[] nums){
-        Set<Integer> Uniques=new HashSet<>();
-        
+        Set<Integer> visited= new HashSet<>();
         for (int i=0;i<nums.length;i++){
-            if(Uniques.contains(nums[i]))
-            {
+            if(visited.contains(nums[i])){
                 return true;
-            }  
-            else 
-                Uniques.add(nums[i]);
+            }
+            visited.add(nums[i]);
         }
         return false;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
