83ec009157d8ef306ca4f1b2260efe4761ac2c74
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2025-06-04 15:12:44 -0700

    Time: 3 ms (22.42%), Space: 55.2 MB (87.26%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,41 +1,23 @@
-public class Solution {
+class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        
-        // Initialize arrays to store products to the left and right of each index
-        int[] leftProducts = new int[n];
-        int[] rightProducts = new int[n];
-        
-        // Initialize result array
-        int[] result = new int[n];
-        
-        // Calculate products to the left of each index
-        leftProducts[0] = 1;
-        for (int i = 1; i < n; i++) {
-            leftProducts[i] = leftProducts[i - 1] * nums[i - 1];
+
+
+        int[] result= new int[nums.length];
+        Arrays.fill(result,1);
+        int pre=1,post=1;
+        for(int i=0;i<nums.length;i++){
+            result[i]=pre;
+            pre=nums[i]*pre;
         }
-        
-        // Calculate products to the right of each index
-        rightProducts[n - 1] = 1;
-        for (int i = n - 2; i >= 0; i--) {
-            rightProducts[i] = rightProducts[i + 1] * nums[i + 1];
+        for (int i=nums.length-1;i>=0;i--){
+            result[i]=result[i]*post;
+            post=post*nums[i];
         }
+
+        return result ;
+
+ 
+
         
-        // Calculate the final result
-        for (int i = 0; i < n; i++) {
-            result[i] = leftProducts[i] * rightProducts[i];
-        }
-        
-        return result;
-    }
-    
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] nums = {1, 2, 3, 4};
-        int[] result = solution.productExceptSelf(nums);
-        for (int num : result) {
-            System.out.print(num + " ");
-        }
-        // Output: 24 12 8 6
     }
 }
