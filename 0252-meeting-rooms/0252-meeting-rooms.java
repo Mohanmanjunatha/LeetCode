8f4a02183cf987a26a6fad718850f1ef57439051
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2026-02-04 21:52:36 -0500

    Time: 6 ms (89.84%), Space: 48.5 MB (59.15%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -3,15 +3,12 @@ class Solution {
         if (intervals == null || intervals.length <= 1) {
             return true;
         }
-
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i - 1][1] > intervals[i][0]) {
                 return false;
             }
         }
-
         return true;
     }
 }
