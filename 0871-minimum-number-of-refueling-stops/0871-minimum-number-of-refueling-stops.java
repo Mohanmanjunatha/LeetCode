138332914f2b7a2864280e1e35c4eff57244b8c3
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2026-02-20 14:26:08 -0800

    Time: 3 ms (90.92%), Space: 47.2 MB (17.99%) - LeetHub

--- 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.java
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.java
@@ -29,25 +29,60 @@
 //     }
 // }
 
+// class Solution {
+//     public int minRefuelStops(int target, int startFuel, int[][] stations) 
+//     {
+//         int ans=0;
+//         PriorityQueue<Integer>pq = new PriorityQueue<>((a,b)->b-a);
+//         int fuel = startFuel;
+//         int n = stations.length;
+//         int index=0;
+//         while(fuel<target)
+//         {
+//             while(index<n && stations[index][0]<=fuel)
+//             {
+//                 pq.add(stations[index][1]);
+//                 index++;
+//             }
+//             if(pq.isEmpty())return -1;
+//             fuel+=pq.poll();
+//             ans++;
+//         }
+//         return ans;
+//     }
+// }
+
+import java.util.*;
+
 class Solution {
-    public int minRefuelStops(int target, int startFuel, int[][] stations) 
-    {
-        int ans=0;
-        PriorityQueue<Integer>pq = new PriorityQueue<>((a,b)->b-a);
-        int fuel = startFuel;
-        int n = stations.length;
-        int index=0;
-        while(fuel<target)
-        {
-            while(index<n && stations[index][0]<=fuel)
-            {
-                pq.add(stations[index][1]);
-                index++;
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        // Max heap for fuels of stations we've passed
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        long fuel = startFuel;
+        int stops = 0;
+        int prev = 0;
+
+        // Traverse all stations + final "station" at target
+        for (int i = 0; i <= stations.length; i++) {
+            int pos = (i == stations.length) ? target : stations[i][0];
+            int gas = (i == stations.length) ? 0 : stations[i][1];
+
+            // spend fuel to reach this position
+            fuel -= (pos - prev);
+
+            // if we can't reach, refuel from best past stations
+            while (fuel < 0) {
+                if (maxHeap.isEmpty()) return -1;
+                fuel += maxHeap.poll();
+                stops++;
             }
-            if(pq.isEmpty())return -1;
-            fuel+=pq.poll();
-            ans++;
+
+            // now we have reached this station, add its gas as an option for later
+            maxHeap.offer(gas);
+            prev = pos;
         }
-        return ans;
+
+        return stops;
     }
 }
