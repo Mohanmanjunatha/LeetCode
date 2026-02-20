71a39f13e216059161a16fa3d9455f71c4812ec5
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2026-02-19 23:29:27 -0800

    Time: 3 ms (90.73%), Space: 47.1 MB (47.15%) - LeetHub

--- 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.java
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.java
@@ -1,30 +1,53 @@
-class Solution {
-    public int minRefuelStops(int target, int tank, int[][] stations) {
-        // pq is a maxheap of gas station capacities
-        PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder());
-        int ans = 0, prev = 0;
-        for (int[] station: stations) {
-            int location = station[0];
-            int capacity = station[1];
-            tank -= location - prev;
-            while (!pq.isEmpty() && tank < 0) {  // must refuel in past
-                tank += pq.poll();
-                ans++;
-            }
-            if (tank < 0) return -1;
-            pq.offer(capacity);
-            prev = location;
-        }
-        // Repeat body for station = (target, inf)
-        {
-            tank -= target - prev;
-            while (!pq.isEmpty() && tank < 0) {
-                tank += pq.poll();
-                ans++;
-            }
-            if (tank < 0) return -1;
-        }
+// class Solution {
+//     public int minRefuelStops(int target, int tank, int[][] stations) {
+//         // pq is a maxheap of gas station capacities
+//         PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder());
+//         int ans = 0, prev = 0;
+//         for (int[] station: stations) {
+//             int location = station[0];
+//             int capacity = station[1];
+//             tank -= location - prev;
+//             while (!pq.isEmpty() && tank < 0) {  // must refuel in past
+//                 tank += pq.poll();
+//                 ans++;
+//             }
+//             if (tank < 0) return -1;
+//             pq.offer(capacity);
+//             prev = location;
+//         }
+//         // Repeat body for station = (target, inf)
+//         {
+//             tank -= target - prev;
+//             while (!pq.isEmpty() && tank < 0) {
+//                 tank += pq.poll();
+//                 ans++;
+//             }
+//             if (tank < 0) return -1;
+//         }
 
+//         return ans;
+//     }
+// }
+
+class Solution {
+    public int minRefuelStops(int target, int startFuel, int[][] stations) 
+    {
+        int ans=0;
+        PriorityQueue<Integer>pq = new PriorityQueue<>((a,b)->b-a);
+        int fuel = startFuel;
+        int n = stations.length;
+        int index=0;
+        while(fuel<target)
+        {
+            while(index<n && stations[index][0]<=fuel)
+            {
+                pq.add(stations[index][1]);
+                index++;
+            }
+            if(pq.isEmpty())return -1;
+            fuel+=pq.poll();
+            ans++;
+        }
         return ans;
     }
 }
