521b058c90a204016853238fc53d911338e434d7
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2026-03-08 00:02:52 -0800

    Time: 2 ms (99.74%), Space: 44.9 MB (77.85%) - LeetHub

--- 1436-destination-city/1436-destination-city.java
+++ 1436-destination-city/1436-destination-city.java
@@ -1,16 +1,35 @@
-class Solution {
-    public String destCity(List<List<String>> paths) {
-        for (int i=0; i<paths.size();i++){
-            String candidate = paths.get(i).get(1);
-            boolean good= true;
-            for (int j=0;j<paths.size();j++){
-                if (paths.get(j).get(0).equals(candidate)){
-                    good = false;
-                    break;
-                }
-            }
-            if (good){
-                return candidate;
+// class Solution {
+//     public String destCity(List<List<String>> paths) {
+//         for (int i=0; i<paths.size();i++){
+//             String candidate = paths.get(i).get(1);
+//             boolean good= true;
+//             for (int j=0;j<paths.size();j++){
+//                 if (paths.get(j).get(0).equals(candidate)){
+//                     good = false;
+//                     break;
+//                 }
+//             }
+//             if (good){
+//                 return candidate;
+//             }
+//         }
+//         return "";
+//     }
+
+
+
+
+
+
+class Solution{
+    public String destCity(List<List<String>> paths){
+        HashSet<String> set = new HashSet<>();
+        for (List<String> path:paths){
+            set.add(path.get(0));
+        }
+        for (List<String> path:paths){
+            if (!set.contains(path.get(1))){
+                return path.get(1);
             }
         }
         return "";
